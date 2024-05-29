0deee505825e6f8819c8bbf0291337b291daf6d7
zillulofficial <zillul.hakim100@gmail.com>
2024-05-29 11:53:43 +0600

    updating part has been established

--- src/Components/CoffeeCards.jsx
+++ src/Components/CoffeeCards.jsx
@@ -2,9 +2,42 @@ import PropTypes from 'prop-types';
 import { MdEdit } from "react-icons/md";
 import { FaEye } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
+import Swal from 'sweetalert2';
+import { Link } from 'react-router-dom';
 
 const CoffeeCards = ({ coffee }) => {
-    const { name, chief, supplier, category, details, url } = coffee
+    const { _id, name, chief, supplier, category, details, url } = coffee
+
+    const handleDelete = id => {
+        console.log(id)
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                fetch(`http://localhost:5000/coffee/${id}`,{
+                    method:"DELETE"
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data)
+                        if(data.deletedCount>0){
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+            }
+        });
+    }
     return (
         <div>
             <div className="card card-side shadow-xl h-64 bg-[#F5F4F1]">
@@ -17,9 +50,9 @@ const CoffeeCards = ({ coffee }) => {
                     <p className='text-sm'>{details}</p>
                 </div>
                 <div className='flex flex-col items-center justify-center mr-6 gap-4'>
-                    <button className='text-lg p-2 rounded-md bg-[#3C393B] text-white'><MdEdit></MdEdit></button>
-                    <button className='text-lg p-2 rounded-md bg-[#EA4744] text-white'><MdDelete></MdDelete></button>
                     <button className='text-lg p-2 rounded-md bg-[#D2B48C] text-white'><FaEye></FaEye></button>
+                    <Link to={`/updateCoffee/${_id}`}><button className='text-lg p-2 rounded-md bg-[#3C393B] text-white'><MdEdit></MdEdit></button></Link>
+                    <button onClick={() => handleDelete(_id)} className='text-lg p-2 rounded-md bg-[#EA4744] text-white'><MdDelete></MdDelete></button>
                 </div>
             </div>
         </div>
--- src/Components/UpdateCoffee.jsx
+++ src/Components/UpdateCoffee.jsx
@@ -1,8 +1,118 @@
+import { useLoaderData } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const UpdateCoffee = () => {
+    const coffee= useLoaderData()
+    const {_id, name, chief, supplier, taste, category, details, url}= coffee
+
+    const handleUpdateCoffee= e=>{
+        e.preventDefault()
+        const form= e.target
+        const name= form.name.value
+        const chief= form.chief.value
+        const supplier= form.supplier.value
+        const taste= form.taste.value
+        const category= form.category.value
+        const details= form.details.value
+        const url= form.url.value
+
+        const updatedCoffee= {name, chief, supplier, taste, category, details, url}
+        console.log(updatedCoffee)
+
+        fetch(`http://localhost:5000/coffee/${_id}`, {
+            method: "PUT",
+            headers:{
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(updatedCoffee)
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data)
+            if(data.modifiedCount>0){
+                Swal.fire({
+                    title: 'Success!',
+                    text: 'Coffee Updated successfully',
+                    icon: 'success',
+                    confirmButtonText: 'Okay'
+                  })
+            }
+        })
+    }
     return (
-        <div>
-            <p>Update a coffee</p>
+        <div className="container mx-auto mt-20">
+            <div className="bg-[#F4F3F0]  py-16 px-28">
+                <div className=" text-center mb-10">
+                    <h3 className="Rancho text-5xl">Update Coffee</h3>
+                    <p>It is a long established fact that a reader will be distracted by the readable
+                        content of a page when looking at<br className="hidden lg:block" /> its layout. The point of using Lorem Ipsum
+                        is that it has a more-or-less normal distribution of letters, as opposed to
+                        using Content here.</p>
+                </div>
+                <form onSubmit={handleUpdateCoffee}>
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                        <div>
+                            <label className="form-control w-full ">
+                                <div className="label">
+                                    <span className="label-text">Name</span>
+                                </div>
+                                <input type="text" name="name" defaultValue={name} placeholder="Enter Coffee Name" className="input input-bordered w-full " />
+                            </label>
+                        </div>
+                        <div>
+                            <label className="form-control w-full ">
+                                <div className="label">
+                                    <span className="label-text">Chief</span>
+                                </div>
+                                <input type="text" name="chief" defaultValue={chief} placeholder="Enter Coffee Chief" className="input input-bordered w-full " />
+                            </label>
+                        </div>
+                        <div>
+                            <label className="form-control w-full ">
+                                <div className="label">
+                                    <span className="label-text">Supplier</span>
+                                </div>
+                                <input type="text" name="supplier" defaultValue={supplier} placeholder="Enter Coffee Supplier" className="input input-bordered w-full " />
+                            </label>
+                        </div>
+                        <div>
+                            <label className="form-control w-full ">
+                                <div className="label">
+                                    <span className="label-text">Taste</span>
+                                </div>
+                                <input type="text" name="taste" defaultValue={taste} placeholder="Enter Coffee Taste" className="input input-bordered w-full " />
+                            </label>
+                        </div>
+                        <div>
+                            <label className="form-control w-full ">
+                                <div className="label">
+                                    <span className="label-text">Category</span>
+                                </div>
+                                <input type="text" name="category" defaultValue={category} placeholder="Enter Coffee Category" className="input input-bordered w-full " />
+                            </label>
+                        </div>
+                        <div>
+                            <label className="form-control w-full ">
+                                <div className="label">
+                                    <span className="label-text">Details</span>
+                                </div>
+                                <input type="text" name="details" defaultValue={details} placeholder="Enter Coffee Details" className="input input-bordered w-full " />
+                            </label>
+                        </div>
+                        <div className="col-span-2">
+                            <label className="form-control w-full ">
+                                <div className="label">
+                                    <span className="label-text">Photo</span>
+                                </div>
+                                <input type="text" name="url" defaultValue={url} placeholder="Enter Coffee URL" className="input input-bordered w-full" />
+                            </label>
+                        </div>
+                    </div>
+                    <div>
+                        <input className="btn Rancho text-2xl text-[#331A15] bg-[#D2B48C] w-full mt-7 hover:bg-[#D2B48C60] border-none" type="submit" value="Update Coffee" />
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
